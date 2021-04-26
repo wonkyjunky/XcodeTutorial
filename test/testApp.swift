4aaa485807a59ccd501cf13092a1be15a45f3901
Won Kyu Jeong <56411475+wonkyjunky@users.noreply.github.com>
2021-04-26 04:02:02 -0700

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Won Kyu Jeong on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
