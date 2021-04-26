01c87e798e8113d7aca2e844e6fbc38fa3537560
Won Kyu Jeong <56411475+wonkyjunky@users.noreply.github.com>
2021-04-26 05:47:40 -0700

    card war game without user interaction

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -6,11 +6,45 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("background").ignoresSafeArea()
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player").foregroundColor(.white)
+                            .font(.headline).padding(.bottom, 10.0)
+                        Text("0").foregroundColor(.white)
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU").foregroundColor(.white)
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0").foregroundColor(.white)
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
     }
 }
 
